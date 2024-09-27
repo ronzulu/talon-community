b50400f8dccda32f4b3130df710e761ab32705a7
ronzulu <75528127+ronzulu@users.noreply.github.com>
2024-09-27 16:54:42 +1000

    progress

--- core/z-modes/z-modes.py
+++ core/z-modes/z-modes.py
@@ -3,19 +3,38 @@ from talon import Module, actions
 
 mod = Module()
 
+global_next_pre_phrase = ""
 global_next_format = ""
+global_subsequent_pre_phrase = ""
 global_subsequent_format = ""
 
 @mod.action_class
 class ZModeActions:
-    def rz_set_format(next_format: str, subsequent_format: str = None):
+    def rz_set_next_format(pre_phrase: str, format: str):
         "Set the format used by following calls to rz_add"
-        global global_next_format, global_subsequent_format
-        global_next_format = next_format
-        global_subsequent_format = subsequent_format or next_format
+        global global_next_pre_phrase, global_next_format
+        global global_subsequent_pre_phrase, global_subsequent_format
+
+        global_next_pre_phrase = pre_phrase
+        global_next_format = format
+        global_subsequent_pre_phrase = pre_phrase
+        global_subsequent_format = format
+
+    def rz_set_subsequent_format(pre_phrase: str, format: str):
+        "Set the format used by following calls to rz_add"
+        global global_subsequent_pre_phrase, global_subsequent_format
+
+        global_subsequent_pre_phrase = pre_phrase
+        global_subsequent_format = format
 
     def rz_add(text: str):
         "Inserts the supplied text, formatted as per last call to rz_set_format"
-        global global_next_format, global_subsequent_format
+        global global_next_pre_phrase, global_next_format
+        global global_subsequent_pre_phrase, global_subsequent_format
+        
+        if global_next_pre_phrase:
+            actions.insert(global_next_pre_phrase)
+
         actions.user.insert_formatted(text, global_next_format)
         global_next_format = global_subsequent_format
+        global_next_pre_phrase = global_subsequent_pre_phrase
--- core/z-modes/z-modes.talon
+++ core/z-modes/z-modes.talon
@@ -2,6 +2,31 @@ hello out there: "Shalom world90214!!"
 
 add <user.text>$: user.rz_add(user.text)
 
-zed one: user.rz_set_format("PUBLIC_CAMEL_CASE")
-zed one l: user.rz_set_format("PRIVATE_CAMEL_CASE", "PUBLIC_CAMEL_CASE")
+zed one:
+    user.rz_set_next_format("", "PUBLIC_CAMEL_CASE")
+zed one l:
+    user.rz_set_next_format("", "PRIVATE_CAMEL_CASE")
+    user.rz_set_subsequent_format("", "PUBLIC_CAMEL_CASE")
+zed one l s:
+    user.rz_set_next_format(" ", "PRIVATE_CAMEL_CASE")
+    user.rz_set_subsequent_format("", "PUBLIC_CAMEL_CASE")
+
+zed two:
+    user.rz_set_next_format("", "NOOP")
+    user.rz_set_subsequent_format(" ", "NOOP")
+
+zed three:
+    user.rz_set_next_format("", "CAPITALIZE_ALL_WORDS")
+    user.rz_set_subsequent_format(" ", "CAPITALIZE_ALL_WORDS")
+zed three s:
+    user.rz_set_next_format(" ", "CAPITALIZE_ALL_WORDS")
+    user.rz_set_subsequent_format(" ", "CAPITALIZE_ALL_WORDS")
+
+zed four:
+    user.rz_set_next_format("", "ALL_CAPS")
+    user.rz_set_subsequent_format(" ", "ALL_CAPS")
+zed four s:
+    user.rz_set_next_format(" ", "ALL_CAPS")
+    user.rz_set_subsequent_format(" ", "ALL_CAPS")
+
 zed ten: user.rz_set_format("DOUBLE_COLON_SEPARATED")
